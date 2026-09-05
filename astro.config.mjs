d1093442acef0d1becc568f09af0474b9ee1c86d
hasanmuammar <mdhasan.c2002@gmail.com>
2026-09-05 16:23:47 +0600

    Correct Astro site URL and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://hasanmuammar.github.io',
-  base: '/lumma-cs.github.io',
+  site: 'https://hasanmuammar.github.io/lumma-cs.github.io/',
+  base: '/lumma-cs.github.io/',
   trailingSlash: 'always',
   publicDir: './assets',
   build: {
